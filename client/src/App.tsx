c14989d3e2fb6e20d58f2fde7821b205260e732c
divyam gupta <divyam7091@gmail.com>
2023-12-31 17:20:28 +0530

    build script added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -42,7 +42,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <button onClick={counter.getCount}>getCount</button>
     </>
   );
 }
